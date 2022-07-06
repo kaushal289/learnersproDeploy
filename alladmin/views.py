a64f02ac272d51519e21e1c2ec1b55965afff8bc
kaushal289 <khanalkaushal9@gmail.com>
2022-07-06 16:40:39 +0545

    bugs fix

--- alladmin/views.py
+++ alladmin/views.py
@@ -84,7 +84,7 @@ def teacherview(request):
         offset = 0
         page = 1
     teachers=Teacher.objects.raw('select * from teacher limit 8 offset %s', [offset])
-    pageItem = len(teachers)%int
+    pageItem = int(len(teachers))
     return render(request,"admin/teacherview.html", {'teachers':teachers,'page': page, 'pageItem': pageItem})
 def studentview(request):
     if (request.method == "POST"):
